5e4e371753054ca1b533643f1213008d18fb5bf1
Tomdieu <ivantomdio@gmail.com>
2025-01-12 18:14:32 +0100

    feat: add UUID field as primary key for User model

--- backend/user-service/core/models.py
+++ backend/user-service/core/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from phonenumber_field.modelfields import PhoneNumberField
+import uuid
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
@@ -39,6 +40,7 @@ class User(AbstractBaseUser, PermissionsMixin):
         ('fr', 'French'),
     ]
     
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(_("email address"), unique=True)
     first_name = models.CharField(_("first name"), max_length=150, blank=True)
     last_name = models.CharField(_("last name"), max_length=150, blank=True)
