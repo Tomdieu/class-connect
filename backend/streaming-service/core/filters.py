f870d9bf59818e5fed6bdbb5330b52e670347ed9
Tomdieu <ivantomdio@gmail.com>
2025-01-13 23:21:59 +0100

    feat : added streaming service

---
+++ backend/streaming-service/core/filters.py
@@ -0,0 +1,17 @@
+import django_filters
+from .models import VideoConferenceSession, SessionParticipant
+
+class VideoConferenceSessionFilter(django_filters.FilterSet):
+    status = django_filters.ChoiceFilter(choices=VideoConferenceSession.SESSION_STATUS)
+    start_time = django_filters.DateTimeFromToRangeFilter()
+    
+    class Meta:
+        model = VideoConferenceSession
+        fields = ['course_id', 'instructor_id', 'status']
+
+class SessionParticipantFilter(django_filters.FilterSet):
+    joined_at = django_filters.DateTimeFromToRangeFilter()
+    
+    class Meta:
+        model = SessionParticipant
+        fields = ['session', 'user_id']
