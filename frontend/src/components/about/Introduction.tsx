b327c3a92050ee233fc209ccbf98af0ec604f761
Tomdieu <ivantomdio@gmail.com>
2025-01-17 18:33:45 +0100

    feat: implement client-side rendering and add internationalization support for about section components

--- frontend/src/components/about/Introduction.tsx
+++ frontend/src/components/about/Introduction.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { useI18n } from "@/locales/client";
 import { motion } from "framer-motion";
 
--- frontend/src/components/about/WhatIsClassConnect.tsx
+++ frontend/src/components/about/WhatIsClassConnect.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { motion } from "framer-motion";
 import { useI18n} from "@/locales/client"
 import Image from "next/image";
