01b3b60bf7f3507d18ef7a45f3dbfeb672727859
Tomdieu <ivantomdio@gmail.com>
2025-01-17 16:09:57 +0100

    feat: add TypeScript definitions for environment variables

---
+++ frontend/src/types/env.d.ts
@@ -0,0 +1,8 @@
+declare namespace NodeJS {
+    export interface ProcessEnv {
+      ACCOUNT_SERVICE: string;
+      COURSE_SERVIVE: string;
+      PAYMENT_SERVICE: string;
+      NOTIFICATION_SERVICE: string;
+    }
+  }
