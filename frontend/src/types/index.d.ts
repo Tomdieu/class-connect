febf2b1d048dcc848bcbfba4f960b5f9a29dcaa4
Tomdieu <ivantomdio@gmail.com>
2025-01-17 13:29:31 +0100

    feat: add UserType interface for user data structure

---
+++ frontend/src/types/index.d.ts
@@ -0,0 +1,24 @@
+interface UserType {
+  id: number;
+  first_name: string;
+  last_name: string;
+  phone_number: string;
+  date_of_birth: string;
+  education_level: string;
+  class_grade: string;
+  email_verified: boolean;
+  profile_picture: string;
+  language: string;
+  town: string;
+  quarter: string;
+  is_staff: boolean;
+  is_active: boolean;
+  email: string;
+  password: string;
+  role: string;
+  created_at: string;
+  updated_at: string;
+  date_joined: string;
+  is_superuser: boolean;
+  last_login: string;
+}
