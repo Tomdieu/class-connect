2a674609a2a53104583b5826b5d53945f05a87b2
Tomdieu <ivantomdio@gmail.com>
2025-01-17 13:32:27 +0100

    feat: add session retrieval function and full name utility for user data

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const getFullName = (user:UserType) => {
+  if(user.first_name !== "" && user.last_name!==""){
+      return user.first_name + " "+ user.last_name
+  }
+  return user.first_name + user.last_name
+}
