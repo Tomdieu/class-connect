9f007ed4a625de23756c62b1ad675d75188de23d
Tomdieu <ivantomdio@gmail.com>
2025-01-17 16:08:50 +0100

    feat: add ChangeLanguage component for language selection

---
+++ frontend/src/components/ChangeLanguage.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+import { useChangeLocale, useCurrentLocale } from '@/locales/client'
+import { Languages } from 'lucide-react'
+
+
+function ChangeLanguage() {
+    const changeLocalLangauge = useChangeLocale()
+    const currentLocale = useCurrentLocale()
+    const [value, setValue] = React.useState(currentLocale)
+
+    const handleChange = (value: "en" | "fr") => {
+        changeLocalLangauge(value)
+        setValue(value)
+    }
+    return (
+        <Select value={value} onValueChange={handleChange}>
+            <SelectTrigger className="w-fit flex items-center gap-1">
+                <Languages className='w-4 h-4 text-muted-foreground'/>
+                <SelectValue placeholder="Lang" className='mr-1' />
+            </SelectTrigger>
+            <SelectContent>
+                <SelectItem value="en">English</SelectItem>
+                <SelectItem value="fr">Français</SelectItem>
+            </SelectContent>
+        </Select>
+
+    )
+}
+
+export default ChangeLanguage
