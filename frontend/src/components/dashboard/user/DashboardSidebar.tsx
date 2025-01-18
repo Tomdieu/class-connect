1c26de7b9d75ca6cb24b1ac0de434635cea93018
Tomdieu <ivantomdio@gmail.com>
2025-01-18 23:09:18 +0100

    feat: localize DashboardSidebar menu items for improved internationalization

--- frontend/src/components/dashboard/user/DashboardSidebar.tsx
+++ frontend/src/components/dashboard/user/DashboardSidebar.tsx
@@ -19,61 +19,61 @@ import {
 } from "@/components/ui/sidebar"
 import { usePathname } from "next/navigation";
 import { getPathAfterLanguage } from "@/lib/utils";
-
-// Menu items.
-const items = [
-  {
-    title: "Home",
-    url: "/dashboard",
-    icon: TiHome,
-  },
-  {
-    title: "Mes eleves",
-    url: "#",
-    icon: FaGraduationCap,
-  },
-  {
-    title: "Mes disponibilités",
-    url: "#",
-    icon: FaCalendarCheck,
-  },
-  {
-    title: "Mes paiements",
-    url: "#",
-    icon: FaEuroSign,
-  },
-  {
-    title: "Mes ressources",
-    url: "#",
-    icon: IoLibrarySharp,
-  },
-  {
-    title: "Nous contacter",
-    url: "#",
-    icon: IoChatbubbles,
-  },
-]
+import { useI18n } from "@/locales/client";
 
 export default function DashboardSidebar() {
   const { toggleSidebar } = useSidebar()
   const pathname = usePathname()
   const correctPath = getPathAfterLanguage(pathname)
+  const t = useI18n();
+
+  const items = [
+    {
+      title: t("dashboardSidebar.home"),
+      url: "/dashboard",
+      icon: TiHome,
+    },
+    {
+      title: t("dashboardSidebar.myStudents"),
+      url: "/dashboard/my-students",
+      icon: FaGraduationCap,
+    },
+    {
+      title: t("dashboardSidebar.myAvailabilities"),
+      url: "/dashboard/my-availabilities",
+      icon: FaCalendarCheck,
+    },
+    {
+      title: t("dashboardSidebar.myPayments"),
+      url: "#",
+      icon: FaEuroSign,
+    },
+    {
+      title: t("dashboardSidebar.myResources"),
+      url: "#",
+      icon: IoLibrarySharp,
+    },
+    {
+      title: t("dashboardSidebar.contactUs"),
+      url: "#",
+      icon: IoChatbubbles,
+    },
+  ];
+
   return (
     <Sidebar collapsible="offcanvas" aria-describedby="dashboard-sidebar" className="bg-white border-none outline-none shadow-lg">
       <SidebarContent aria-labelledby="x" aria-describedby="dashboard-sidebar" className="bg-white border-none outline-none">
         <SidebarGroup>
           <div className="flex items-center justify-between mb-4">
-
-            <SidebarGroupLabel className="font-bold text-xl">Menu</SidebarGroupLabel>
-
-            <div onClick={toggleSidebar} className="cursor-pointer">
+            <SidebarGroupLabel className="font-bold text-xl">{t("dashboardSidebar.menu")}</SidebarGroupLabel>
+            <div onClick={toggleSidebar} className="cursor-pointer sm:hidden">
               <X className="text-muted-foreground size-6"/>
             </div>
           </div>
           <SidebarGroupContent>
             <SidebarMenu>
               {items.map((item) => (
-                <SidebarMenuItem key={item.title} className={`rounded-md hover:bg-default/15 ${correctPath === item.url ? "bg-indigo-500" : ""}`}>
+                <SidebarMenuItem key={item.title} className={`rounded-md hover:bg-default/15 ${correctPath === item.url ? "bg-indigo-500 text-white" : ""}`}>
                   <SidebarMenuButton asChild>
                     <a href={item.url} className="w-full flex items-center gap-2 p-2 text-sm outline-none ring-sidebar-ring transition-[width,height,padding] hover:bg-default/15">
                       <item.icon className="w-8 h-8"/>
