1c9af00353aa33c4701a6f225bfb696874094e2e
Tomdieu <ivantomdio@gmail.com>
2025-01-21 23:44:36 +0100

    refactor: remove unused backend files and configurations

--- backend/course-service/core/permisions.py
+++
@@ -1,5 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class IsProfessional(BasePermission):
-    def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.education_level == 'PROFESSIONAL'
--- backend/course-service/core/signals.py
+++
@@ -1,9 +0,0 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from .models import UserAvailability
-
-@receiver(post_save, sender=UserAvailability)
-def create_time_slots(sender, instance:UserAvailability, created, **kwargs):
-    """Signal to create time slots when availability is created"""
-    if created and instance.is_available:
-        instance.create_default_slots()
