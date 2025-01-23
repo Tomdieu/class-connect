f9bb005e43498fa01574a808c6352e8662672f73
Tomdieu <ivantomdio@gmail.com>
2025-01-23 10:08:06 +0100

    feat: enhance User model with education level specific fields and validation

--- backend/monolythic/users/models.py
+++ backend/monolythic/users/models.py
@@ -29,10 +29,36 @@ class UserManager(BaseUserManager):
 
 class User(AbstractBaseUser, PermissionsMixin):
     EDUCATION_LEVELS = [
-        ('COLLEGE', 'Collège'),
         ('LYCEE', 'Lycée'),
         ('UNIVERSITY', 'Université'),
         ('PROFESSIONAL', 'Professionnel'),
+    ]
+
+    LYCEE_CLASSES = [
+        ('6eme', '6ème'),
+        ('5eme', '5ème'),
+        ('4eme', '4ème'),
+        ('3eme', '3ème'),
+        ('2nde', '2nde'),
+        ('1ere', '1ère'),
+        ('terminale', 'Terminale'),
+    ]
+
+    UNIVERSITY_LEVELS = [
+        ('licence', 'Licence'),
+        ('master', 'Master'),
+        ('doctorat', 'Doctorat'),
+    ]
+
+    LICENCE_YEARS = [
+        ('L1', 'L1'),
+        ('L2', 'L2'),
+        ('L3', 'L3'),
+    ]
+
+    MASTER_YEARS = [
+        ('M1', 'M1'),
+        ('M2', 'M2'),
     ]
     
     LANGUAGE_CHOICES = [
@@ -42,17 +68,29 @@ class User(AbstractBaseUser, PermissionsMixin):
     
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(_("email address"), unique=True)
-    first_name = models.CharField(_("first name"), max_length=150, blank=True)
-    last_name = models.CharField(_("last name"), max_length=150, blank=True)
+    first_name = models.CharField(_("first name"), max_length=150)
+    last_name = models.CharField(_("last name"), max_length=150)
     phone_number = PhoneNumberField(unique=True, region='CM')
-    date_of_birth = models.DateField(null=True)
+    date_of_birth = models.DateField(null=True,blank=True)
     education_level = models.CharField(max_length=20, choices=EDUCATION_LEVELS)
-    class_grade = models.CharField(max_length=50, blank=True)
+    
+    # Education level specific fields
+    lycee_class = models.CharField(max_length=20, choices=LYCEE_CLASSES, null=True, blank=True)
+    university_level = models.CharField(max_length=20, choices=UNIVERSITY_LEVELS, null=True, blank=True)
+    university_year = models.CharField(
+        max_length=2, 
+        choices=LICENCE_YEARS + MASTER_YEARS, 
+        null=True, 
+        blank=True
+    )
+    enterprise_name = models.CharField(max_length=255, null=True, blank=True)
+    platform_usage_reason = models.TextField(null=True, blank=True)
+    
     email_verified = models.BooleanField(default=False)
     profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
     language = models.CharField(max_length=2, choices=LANGUAGE_CHOICES, default='fr')
-    town = models.CharField(max_length=100, blank=True,null=True)
-    quarter = models.CharField(max_length=100, blank=True,null=True)
+    town = models.CharField(max_length=100,blank=True,null=True)
+    quarter = models.CharField(max_length=100,blank=True,null=True)
     
     is_staff = models.BooleanField(
         _("staff status"),
@@ -71,7 +109,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'phone_number']
 
     class Meta:
         verbose_name = _("User")
@@ -79,6 +117,38 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+    def clean(self):
+        from django.core.exceptions import ValidationError
+        
+        # Validate education level specific fields
+        if self.education_level == 'LYCEE':
+            if not self.lycee_class:
+                raise ValidationError(_('Lycee class is required for lycee students'))
+            self.university_level = None
+            self.university_year = None
+            self.enterprise_name = None
+            self.platform_usage_reason = None
+            
+        elif self.education_level == 'UNIVERSITY':
+            if not self.university_level:
+                raise ValidationError(_('University level is required for university students'))
+            if self.university_level in ['licence', 'master'] and not self.university_year:
+                raise ValidationError(_('University year is required for licence and master students'))
+            self.lycee_class = None
+            self.enterprise_name = None
+            self.platform_usage_reason = None
+            
+        elif self.education_level == 'PROFESSIONAL':
+            if not self.enterprise_name or not self.platform_usage_reason:
+                raise ValidationError(_('Enterprise name and platform usage reason are required for professionals'))
+            self.lycee_class = None
+            self.university_level = None
+            self.university_year = None
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
 
 class UserPasswordResetToken(models.Model):
     user = models.ForeignKey(User, related_name="reset_token", on_delete=models.CASCADE)
