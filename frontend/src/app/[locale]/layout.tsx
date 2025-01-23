3c7c02db1cab7f6ecf38bcabddec87c09ed8504d
Tomdieu <ivantomdio@gmail.com>
2025-01-23 10:12:52 +0100

    feat: enhance layout with custom font integration and improved metadata for SEO

--- frontend/src/app/[locale]/layout.tsx
+++ frontend/src/app/[locale]/layout.tsx
@@ -5,12 +5,59 @@ import { ThemeProvider } from "@/components/theme-provider";
 import Modals from "@/components/modals";
 import { Providers } from "./providers";
 
+import localFont from "next/font/local";
+import { SessionProvider } from "next-auth/react";
+
+const Inter = localFont({
+  src: [
+    {
+      path: "../../../public/fonts/Inter/static/Inter-Regular.ttf",
+      weight: "400",
+    },
+    {
+      path: "../../../public/fonts/Inter/static/Inter-Medium.ttf",
+      weight: "500",
+    },
+    {
+      path: "../../../public/fonts/Inter/static/Inter-SemiBold.ttf",
+      weight: "600",
+    },
+    {
+      path: "../../../public/fonts/Inter/static/Inter-Bold.ttf",
+      weight: "700",
+    },
+    {
+      path: "../../../public/fonts/Inter/static/Inter-Black.ttf",
+      weight: "800",
+    },
+    {
+      path: "../../../public/fonts/Inter/static/Inter-ExtraBold.ttf",
+      weight: "900",
+    },
+  ],
+  variable: "--font-inter",
+  display:"swap"
+});
+
+
 
 export const metadata: Metadata = {
   title: "ClassConnect",
   description: "Apprenez à votre rythme avec ClassConnect",
   keywords: "ClassConnect, apprendre, tutoriel, cours, e-learning, formation, enseignement, éducation, apprentissage, tutoriel, cours, e-learning, formation, enseignement, éducation",
-  robots: "index,follow",
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
   openGraph: {
     type: "website",
     locale: "fr_FR",
@@ -29,6 +76,33 @@ export const metadata: Metadata = {
     alternateLocale: ['en', 'fr'],
     countryName: "Cameroon",
   },
+  twitter:{
+    card:"summary_large_image",
+    title:"ClassConnect",
+    description:"Apprenez à votre rythme avec ClassConnect",
+    site:"@classconnect",
+    creator:"@tomdieuivan",
+    images:[
+      {
+        url: "https://class-connect-alpha.vercel.app/icon.svg",
+        width: 1200,
+        height: 630,
+        alt: "ClassConnect"
+      }
+    ],
+  },
+  metadataBase:new URL('https://class-connect-alpha.vercel.app'),
+  creator:"Tomdieu Ivan",
+  category:"Education",
+  icons:[
+    {
+      rel:"icon",
+      href:"/icon.svg",
+      type:"image/svg",
+      sizes:"32x32",
+      url:"/icon.svg"
+    }
+  ]
 };
 
 export default async function RootLayout({
@@ -44,7 +118,7 @@ export default async function RootLayout({
   return (
     <html lang={locale} suppressHydrationWarning>
       <body
-        className={`antialiased overflow-y-auto flex flex-col`}
+        className={`antialiased overflow-y-auto flex flex-col ${Inter.variable} font-inter`}
         suppressHydrationWarning
       >
         <ThemeProvider
@@ -52,12 +126,15 @@ export default async function RootLayout({
           defaultTheme="light"
           disableTransitionOnChange
         >
+        <SessionProvider>
+
           <Providers locale={locale}>
 
             {children}
             <Modals />
             <Toaster />
           </Providers>
+        </SessionProvider>
         </ThemeProvider>
       </body>
     </html>
