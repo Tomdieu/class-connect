c185b2765f3948e4a26e7a3df0db2b3ebcb6dd76
Tomdieu <ivantomdio@gmail.com>
2025-01-23 10:13:28 +0100

    feat: enhance Header component with className prop and responsive design adjustments

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -7,26 +7,30 @@ import { useAuthDialog } from '@/hooks/use-auth-dialog'
 // import { ModeToggle } from './ModeToggle';
 import { useMediaQuery } from 'usehooks-ts'
 import ChangeLanguage from './ChangeLanguage';
+import { cn } from '@/lib/utils';
 
-function Header() {
+function Header({className}:{className?:string}) {
     const { openLogin, openRegister } = useAuthDialog()
     const isMobile = useMediaQuery('(max-width: 640px)')
     return (
-        <div className='flex flex-wrap items-center justify-between py-5 px-5'>
+        <div className={cn('flex items-center justify-between py-5',className)}>
             <Link href={"/"} className='select-none flex items-center gap-1 cursor-pointer text-blue-600'>
-                <BookOpen className='size-8' />
-                <h1 className='font-black text-lg sm:text-lg'>ClassConnect</h1>
+                <BookOpen className='size-6 sm:size-8' />
+                <h1 className='font-black text-base sm:text-lg text-blue-600'>ClassConnect</h1>
             </Link>
             <div className='flex items-center gap-1 2xl:gap-5'>
-                <Button size={isMobile ? "default" : "default"} onClick={openLogin} className='bg-white text-black hover:text-blue-600 hover:bg-blue-100 border border-gray-200'>
-                    <LogIn />
+                <Button variant={"outline"} size={isMobile ? "default" : "default"} onClick={openLogin} className='text-black hover:text-blue-600 hover:bg-primary/10 border'>
+                    <LogIn className='hidden'/>
                     <span className='text-xs sm:text-sm'>Se Connecter</span>
                 </Button>
                 <Button size={isMobile ? "default" : "default"} onClick={openRegister} className='bg-default text-sm hover:bg-default/80 text-white'>
-                    <UserPlus />
+                    <UserPlus className='hidden'/>
                     <span className='text-xs sm:text-sm'>S&apos;inscrire</span>
                 </Button>
+                {!isMobile && (
+
                 <ChangeLanguage />
+                )}
                 {/* <ModeToggle/> */}
             </div>
         </div>
