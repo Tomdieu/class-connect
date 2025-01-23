bcdae5e3a5f0d2c9f8d310e26a3b6388ab1eebbb
Tomdieu <ivantomdio@gmail.com>
2025-01-23 10:09:48 +0100

    feat: implement NextAuth authentication with credentials provider

---
+++ frontend/src/auth.ts
@@ -0,0 +1,94 @@
+import NextAuth, { User } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { LoginResponseType } from "./types";
+import { getAccountInfor } from "./actions/accounts";
+
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  providers: [
+    Credentials({
+      name: "credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "xyz@gmail.com",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "************",
+        },
+      },
+      async authorize(credentials) {
+        console.log(credentials);
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Invalid Credentials");
+        }
+
+        const body = {
+          username: credentials.email,
+          password: credentials.password,
+          grant_type: "password",
+          client_id: process.env.NEXT_CLASS_CONNECT_CLIENT_ID!,
+          client_secret: process.env.NEXT_CLASS_CONNECT_CLIENT_SECRETE!,
+        };
+
+        const res = await fetch(
+          process.env.NEXT_PUBLIC_BACKEND_URL + "/api/auth/token/",
+          {
+            method: "POST",
+            body: JSON.stringify(body),
+            headers: { "Content-Type": "application/json" },
+          }
+        );
+
+        const data = (await res.json()) as LoginResponseType;
+
+        console.log(data);
+        if (res.ok && !data.access_token) {
+          return null;
+        }
+
+        if (res.ok && data.access_token) {
+          const expires_in = data.expires_in; // in seconds
+          // calculate when the token expires to know when to logout the user when it expires
+          const expiresAt = Date.now() + expires_in * 1000; // convert to milliseconds
+
+          const user = await getAccountInfor(data.access_token);
+
+          return {
+            ...user,
+            email_verified:user.email_verified,
+            accessToken: data.access_token,
+            refreshToken: data.refresh_token,
+            expiresAt,
+          } as User;
+        }
+        return null;
+      },
+    }),
+  ],
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) {
+        token = { ...token, ...user };
+      }
+      return token;
+    },
+    session: ({ session, token }) => {
+      delete token.exp;
+      delete token.iat;
+      delete token.sub;
+      delete token.jti;
+      delete token.picture;
+      delete token.user;
+      session.user = token;
+
+      return session;
+    },
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.AUTH_SECRET,
+});
