1b29fe489b7bfc6a9cbceaa0ea6d249970727cb3
Tomdieu <ivantomdio@gmail.com>
2025-01-23 10:13:09 +0100

    feat: add UserParams interface for user-related data management

---
+++ frontend/src/types/params.d.ts
@@ -0,0 +1,11 @@
+declare interface UserParams {
+    email?:string;
+    first_name?:string,
+    last_name?:string,
+    phone_number?:string,
+    education_level?:string,
+    is_active?:boolean,
+    date_joined?:string,
+    language?:string,
+    email_verified?:boolean
+}
