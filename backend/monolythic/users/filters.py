6d4e3a93332b26a41d39c0c91c05538558d1b950
Tomdieu <ivantomdio@gmail.com>
2025-01-24 23:30:59 +0100

    feat: extend UserFilter to include additional fields for user information

--- backend/monolythic/users/filters.py
+++ backend/monolythic/users/filters.py
@@ -7,9 +7,18 @@ class UserFilter(django_filters.FilterSet):
     last_name = django_filters.CharFilter(lookup_expr='icontains')
     phone_number = django_filters.CharFilter(lookup_expr='icontains')
     education_level = django_filters.ChoiceFilter(choices=User.EDUCATION_LEVELS)
+    lycee_class = django_filters.CharFilter(lookup_expr='icontains')
+    university_level = django_filters.CharFilter(lookup_expr='icontains')
+    university_year = django_filters.CharFilter(lookup_expr='icontains')
+    enterprise_name = django_filters.CharFilter(lookup_expr='icontains')
+    platform_usage_reason = django_filters.CharFilter(lookup_expr='icontains')
     is_active = django_filters.BooleanFilter()
     date_joined = django_filters.DateFromToRangeFilter()
 
     class Meta:
         model = User
-        fields = ['email', 'first_name', 'last_name', 'phone_number', 'education_level', 'is_active', 'date_joined']
+        fields = [
+            'email', 'first_name', 'last_name', 'phone_number', 'education_level', 
+            'lycee_class', 'university_level', 'university_year', 'enterprise_name', 
+            'platform_usage_reason', 'is_active', 'date_joined'
+        ]
