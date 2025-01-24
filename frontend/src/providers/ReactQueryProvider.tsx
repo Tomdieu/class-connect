4ceb736c03efdf9ada4d4db60ba135ca4961b54e
Tomdieu <ivantomdio@gmail.com>
2025-01-25 00:37:15 +0100

    feat: add ReactQueryProvider component for managing server state with React Query

---
+++ frontend/src/providers/ReactQueryProvider.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import {
+    QueryClient,
+    QueryClientProvider,
+} from '@tanstack/react-query'
+// import {ReactQueryDevtools} from "@tanstack/react-query-devtools"
+import React from 'react'
+
+type Props = {
+    children: React.ReactNode
+}
+
+// Create a client
+const queryClient = new QueryClient()
+
+const ReactQueryProvider = ({ children }: Props) => {
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+            {/* <ReactQueryDevtools client={queryClient}/> */}
+        </QueryClientProvider>
+    )
+}
+
+export default ReactQueryProvider
