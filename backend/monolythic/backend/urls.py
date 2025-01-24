d721f1dbfe3185989fd42a5f6fd9bcf70255ec26
Tomdieu <ivantomdio@gmail.com>
2025-01-24 10:03:58 +0100

    feat: add API documentation and routing for user, course, payment, notification, and streaming services

--- backend/monolythic/backend/urls.py
+++ backend/monolythic/backend/urls.py
@@ -15,8 +15,49 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include,re_path
+
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="ClassConnect API",
+        default_version="v1",
+        description="API for ClassConnect",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="ivan.tomdieu@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+     path('api/', include([
+        path('', include('users.urls')),
+        path('',include('courses.urls')),
+        path('',include('payments.urls')),
+        path('',include('notifications.urls')),
+        path('',include('streamings.urls')),
+        
+        re_path(r'^auth/', include('drf_social_oauth2.urls', namespace='drf')),
+        path(
+        "docs/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    ])),
+    path(
+        "swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"
+    ),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
