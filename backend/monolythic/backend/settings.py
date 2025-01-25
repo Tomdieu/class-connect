c9283666d976928ee28907938d7274d78e23aea3
Tomdieu <ivantomdio@gmail.com>
2025-01-25 22:20:16 +0100

    fix: update Redis cache location to use localhost instead of service name

--- backend/monolythic/backend/settings.py
+++ backend/monolythic/backend/settings.py
@@ -232,7 +232,7 @@ GOOGLE_SERVICE_ACCOUNT_FILE = os.getenv("GOOGLE_SERVICE_ACCOUNT_FILE", BASE_DIR 
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://redis:6379/1',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
