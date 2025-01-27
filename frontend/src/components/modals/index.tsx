94fcc74dcc6c0b167e141da242368f919b71ed54
Tomdieu <ivantomdio@gmail.com>
2025-01-27 21:16:35 +0100

    feat: add UserDialog, SubjectModal, and ChapterModal to Modals component

--- frontend/src/components/modals/index.tsx
+++ frontend/src/components/modals/index.tsx
@@ -2,12 +2,18 @@
 import React from 'react'
 import LoginDialog from './auth/LoginDialog'
 import RegisterDialog from './auth/RegisterDialog'
+import UserDialog from '../dashboard/admin/modals/UserModal'
+import SubjectModal from '../dashboard/admin/modals/SubjectModal'
+import ChapterModal from '../dashboard/admin/modals/ChapterModal'
 
 function Modals() {
   return (
     <>
     <LoginDialog/>
     <RegisterDialog/>
+    <UserDialog/>
+    <SubjectModal/>
+    <ChapterModal/>
     </>
   )
 }
