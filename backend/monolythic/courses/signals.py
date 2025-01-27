d336e5f17424e9bc1c3f3a1f418bc6e0b4519cf5
Tomdieu <ivantomdio@gmail.com>
2025-01-27 21:12:43 +0100

    feat: add cache invalidation signals for Class and Subject models

--- backend/monolythic/courses/signals.py
+++ backend/monolythic/courses/signals.py
@@ -1,9 +1,31 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save,post_delete
 from django.dispatch import receiver
-from .models import UserAvailability
+from .models import UserAvailability,Class,Subject
+from django.core.cache import cache
 
 @receiver(post_save, sender=UserAvailability)
 def create_time_slots(sender, instance:UserAvailability, created, **kwargs):
     """Signal to create time slots when availability is created"""
     if created and instance.is_available:
         instance.create_default_slots()
+
+# invalidate caches when ever we add or remove a  class,subject
+@receiver([post_save,post_delete],sender=Class)
+def invalidate_class_cache(sender,instance,**kwargs):
+    
+    if instance.id:
+        # invalidate user_info cache
+        cache.delete_pattern(f'*class_detail*')
+    
+    # TODO
+    cache.delete_pattern('*class_list*')
+    
+@receiver([post_save,post_delete],sender=Subject)
+def invalidate_subjects_cache(sender,instance,**kwargs):
+    
+    if instance.id:
+        # invalidate user_info cache
+        cache.delete_pattern(f'*subjects_list*')
+    
+    # TODO
+    cache.delete_pattern('*class_list*')
