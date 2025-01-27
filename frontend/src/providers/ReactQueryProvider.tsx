986930add2e97fe1aa0292dc20a1c946677de4e0
Tomdieu <ivantomdio@gmail.com>
2025-01-27 21:15:18 +0100

    feat: enable React Query Devtools for improved debugging

--- frontend/src/providers/ReactQueryProvider.tsx
+++ frontend/src/providers/ReactQueryProvider.tsx
@@ -4,7 +4,7 @@ import {
     QueryClient,
     QueryClientProvider,
 } from '@tanstack/react-query'
-// import {ReactQueryDevtools} from "@tanstack/react-query-devtools"
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools"
 import React from 'react'
 
 type Props = {
@@ -18,7 +18,7 @@ const ReactQueryProvider = ({ children }: Props) => {
     return (
         <QueryClientProvider client={queryClient}>
             {children}
-            {/* <ReactQueryDevtools client={queryClient}/> */}
+            <ReactQueryDevtools client={queryClient}/>
         </QueryClientProvider>
     )
 }
