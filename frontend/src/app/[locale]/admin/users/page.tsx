d2aee0d9c34aefd275bf60e6397bfdfcade1e232
Tomdieu <ivantomdio@gmail.com>
2025-01-27 21:19:54 +0100

    fix: format code in UsersPage component for consistency

--- frontend/src/app/[locale]/admin/users/page.tsx
+++ frontend/src/app/[locale]/admin/users/page.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import UserTable from './user-table'
+import React from "react";
+import UserTable from "./user-table";
 
 async function UsersPage() {
   return (
-    <div className='flex flex-col gap-5 px-2 sm:container pt-10'>
-      <UserTable/>
+    <div className="flex flex-col gap-5 px-2 sm:container pt-10">
+      
+      <UserTable />
     </div>
-  )
+  );
 }
 
-export default UsersPage
+export default UsersPage;
