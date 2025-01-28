df64a63a00494689fd3cb6a6b054c94e9f75a7e0
Tomdieu <ivantomdio@gmail.com>
2025-01-29 00:00:05 +0100

    feat: implement useTopicStore hook for managing topic state and actions

---
+++ frontend/src/hooks/topic-store.tsx
@@ -0,0 +1,49 @@
+import { TopicType } from "@/types";
+import { create } from "zustand";
+
+type TopicStore = {
+  topic?: TopicType;
+  isOpen: boolean;
+  chapterId?: string | number;
+  subjectId?: string | number;
+  classId?: string | number;
+  onAdd: ({
+    chapterId,
+    classId,
+    subjectId,
+  }: {
+    classId: string | number;
+    subjectId: string | number;
+    chapterId: string | number;
+  }) => void;
+  setTopic: ({
+    classId,
+    subjectId,
+    topic,
+  }: {
+    topic: TopicType;
+    subjectId: string | number;
+    classId: string | number;
+  }) => void;
+  onClose: () => void;
+};
+
+export const useTopicStore = create<TopicStore>((set) => ({
+  topic: undefined,
+  isOpen: false,
+  chapterId: undefined,
+  subjectId: undefined,
+  classId: undefined,
+  onAdd: ({ chapterId, classId, subjectId }) =>
+    set({ isOpen: true, chapterId, classId, subjectId }),
+  setTopic: ({ classId, subjectId, topic }) =>
+    set({ topic: topic, isOpen: true, classId, subjectId }),
+  onClose: () =>
+    set({
+      isOpen: false,
+      topic: undefined,
+      chapterId: undefined,
+      subjectId: undefined,
+      classId: undefined,
+    }),
+}));
