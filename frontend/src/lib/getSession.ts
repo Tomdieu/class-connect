54ec273029bc7b2598089b61582a522654fa0b87
Tomdieu <ivantomdio@gmail.com>
2025-01-31 00:13:54 +0100

    feat: remove getSession utility as it is no longer needed

--- frontend/src/lib/getSession.ts
+++
@@ -1,7 +0,0 @@
-"use server"
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/options";
-
-export async function getSession(){
-    return await getServerSession(authOptions);
-}
