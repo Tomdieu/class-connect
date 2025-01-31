bf8bee11e6f67666c55ffa353e820d75a9f97d40
Tomdieu <ivantomdio@gmail.com>
2025-01-31 22:23:44 +0100

    style: format code for consistency in LandingPage component

--- frontend/src/app/[locale]/page.tsx
+++ frontend/src/app/[locale]/page.tsx
@@ -1,17 +1,17 @@
 "use client";
-import Header from '@/components/Header'
-import Hero from '@/components/Hero'
-import { SubscriptionPlans } from '@/components/SubscriptionPlans'
-import React from 'react'
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import { SubscriptionPlans } from "@/components/SubscriptionPlans";
+import React from "react";
 
 function LandingPage() {
   return (
-    <div className='relative flex-1 w-full h-full flex flex-col px-3 sm:container mx-auto'>
-      <Header className=' bg-white'/>
-      <Hero/>
-      <SubscriptionPlans/>
+    <div className="relative flex-1 w-full h-full flex flex-col px-3 sm:container mx-auto">
+      <Header className=" bg-white" />
+      <Hero />
+      <SubscriptionPlans />
     </div>
-  )
+  );
 }
 
-export default LandingPage
+export default LandingPage;
