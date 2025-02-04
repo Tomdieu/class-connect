8aea6ca5a84a1423a3cb40307bc3d647786080ba
Tomdieu <ivantomdio@gmail.com>
2025-02-04 17:10:32 +0100

    refactor: simplify ProtectedLayout component by removing unused session handling code

--- frontend/src/app/[locale]/(protected)/layout.tsx
+++ frontend/src/app/[locale]/(protected)/layout.tsx
@@ -1,22 +1,9 @@
 "use client";
-import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
-import React, { PropsWithChildren, useEffect } from 'react'
+import React, { PropsWithChildren } from "react";
 
 function ProtectedLayout({ children }: PropsWithChildren) {
-    const { data: session } = useSession()
-    console.log({session})
-    // const router = useRouter()
-    // useEffect(() => {
-    //     if (!session?.user) {
-    //         router.push('/auth/login')
-    //     }
-    // }, [session])
-    return (
-        <React.Fragment>
-            {children}
-        </React.Fragment>
-    )
+    
+  return <React.Fragment>{children}</React.Fragment>;
 }
 
-export default ProtectedLayout
+export default ProtectedLayout;
