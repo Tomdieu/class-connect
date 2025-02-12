a835128f1dec23f3904782064f8a4c83f0020a78
Tomdieu <ivantomdio@gmail.com>
2025-02-12 10:45:46 +0100

    feat: enhance PublicChatConsumer to store messages in the database and retrieve the public forum

--- backend/monolythic/forum/consumer.py
+++ backend/monolythic/forum/consumer.py
@@ -1,5 +1,6 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
 
 class PublicChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -10,16 +11,32 @@ class PublicChatConsumer(AsyncWebsocketConsumer):
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
     
+    @database_sync_to_async
+    def get_public_forum(self):
+        from forum.models import Forum
+        forum, _ = Forum.objects.get_or_create(name="Public Forum")
+        return forum
+
+    @database_sync_to_async
+    def store_message(self, forum, sender, content):
+        from forum.models import Messages
+        return Messages.objects.create(forum=forum, sender=sender, content=content)
+
     async def receive(self, text_data):
         data = json.loads(text_data)
-        message = data.get('message')
-        username = data.get('username')
-        # Broadcast message to the group
+        message_text = data.get('message')
+        user = self.scope.get("user")
+        forum = await self.get_public_forum()
+        if user and user.is_authenticated:
+            await self.store_message(forum, user, message_text)
+            username = user.username
+        else:
+            username = "Anonymous"
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message,
+                'message': message_text,
                 'username': username,
             }
         )
