bd19bbddfe0895cbdb278e7297b9d08d8d5d943f
Tomdieu <ivantomdio@gmail.com>
2025-02-12 10:47:40 +0100

    feat: configure CHANNEL_LAYERS for Redis support in settings

--- backend/monolythic/backend/settings.py
+++ backend/monolythic/backend/settings.py
@@ -329,6 +329,16 @@ CACHES = {
     }
 }
 
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
+
+
 
 # AWS S3 Configuration
 
