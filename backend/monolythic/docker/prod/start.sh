c12b12f12cc28fa6883181398dbe70bc7a34b472
Tomdieu <ivantomdio@gmail.com>
2025-02-14 05:52:33 +0100

    chore: update start.sh to switch from gunicorn to daphne for ASGI application

--- backend/monolythic/docker/prod/start.sh
+++ backend/monolythic/docker/prod/start.sh
@@ -8,5 +8,5 @@ python3 manage.py migrate --no-input
 python3 manage.py collectstatic --no-input
 python3 manage.py create_classes
 
-#daphne -b 0.0.0.0 -p 8000 backend.asgi:application
-gunicors backend.wsgi --bind 0.0.0.0:8001 --chdir=/app
+daphne -b 0.0.0.0 -p 8001 backend.asgi:application
+# gunicon backend.wsgi --bind 0.0.0.0:8001 --chdir=/app
