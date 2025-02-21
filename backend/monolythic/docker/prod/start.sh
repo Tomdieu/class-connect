0bc694a208f876342f9cc3fb9e476bde64ef985e
Tomdieu <ivantomdio@gmail.com>
2025-02-21 16:08:26 +0000

    feat: add command to create admin users during production startup

--- backend/monolythic/docker/prod/start.sh
+++ backend/monolythic/docker/prod/start.sh
@@ -7,6 +7,7 @@ python3 manage.py makemigrations --no-input
 python3 manage.py migrate --no-input
 python3 manage.py collectstatic --no-input
 python3 manage.py create_classes
+python manage.py create_admin_users
 
 python3 run_daphne.py
 # daphne -b 0.0.0.0 -p 8001 backend.asgi:application
