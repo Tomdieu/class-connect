bf9e3f28e1a3327673e679658ee25b8ec79c9bb1
Tomdieu <ivantomdio@gmail.com>
2025-02-23 15:54:46 +0100

    refactor: clean up ReactQueryProvider component with consistent formatting and improved readability

--- frontend/src/providers/ReactQueryProvider.tsx
+++ frontend/src/providers/ReactQueryProvider.tsx
@@ -1,26 +1,23 @@
-"use client"
+"use client";
 
-import {
-    QueryClient,
-    QueryClientProvider,
-} from '@tanstack/react-query'
-import {ReactQueryDevtools} from "@tanstack/react-query-devtools"
-import React from 'react'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import React from "react";
 
 type Props = {
-    children: React.ReactNode
-}
+  children: React.ReactNode;
+};
 
 // Create a client
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 const ReactQueryProvider = ({ children }: Props) => {
-    return (
-        <QueryClientProvider client={queryClient}>
-            {children}
-            <ReactQueryDevtools client={queryClient}/>
-        </QueryClientProvider>
-    )
-}
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools client={queryClient} />
+    </QueryClientProvider>
+  );
+};
 
-export default ReactQueryProvider
+export default ReactQueryProvider;
