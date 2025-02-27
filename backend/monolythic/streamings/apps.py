8cada2349526ca5f134c704b0d1b88ca02bb8675
Tomdieu <ivantomdio@gmail.com>
2025-02-27 18:09:40 +0100

    feat: import signals in StreamingsConfig when the app is ready

--- backend/monolythic/streamings/apps.py
+++ backend/monolythic/streamings/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class StreamingsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'streamings'
+
+    def ready(self):
+        """Import signals when the app is ready"""
+        import streamings.signals
