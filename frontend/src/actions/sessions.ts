c228654b7ea8ccba167de61b8ecf82054a5520a2
Tomdieu <ivantomdio@gmail.com>
2025-02-28 22:55:50 +0100

    feat: add session retrieval and error handling for listSessions and getSession functions

--- frontend/src/actions/sessions.ts
+++ frontend/src/actions/sessions.ts
@@ -1,6 +1,7 @@
 "use server";
 import { auth } from "@/auth";
 import api from "@/services/api";
+import { VideoConferenceSessionType } from "@/types";
 import { AxiosError } from "axios";
 
 export const listSessions = async () => {
@@ -12,7 +13,26 @@ export const listSessions = async () => {
         Authorization: `Bearer ${session.user.accessToken}`,
       },
     });
-    return response.data;
+    return response.data as VideoConferenceSessionType[];
+  } catch (error: unknown) {
+    const axiosError = error as AxiosError;
+    if (axiosError.response?.data) {
+      throw JSON.stringify(axiosError.response.data);
+    }
+    throw JSON.stringify({ message: "An unexpected error occurred" });
+  }
+};
+
+export const getSession = async (id: number) => {
+  try {
+    const session = await auth();
+    if (!session?.user) throw Error("Unauthorize user!");
+    const response = await api.get(`/api/sessions/${id}/`, {
+      headers: {
+        Authorization: `Bearer ${session.user.accessToken}`,
+      },
+    });
+    return response.data as VideoConferenceSessionType;
   } catch (error: unknown) {
     const axiosError = error as AxiosError;
     if (axiosError.response?.data) {
