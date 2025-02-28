b6f912c40ce325fac7e4cd8e51741cbff828f7c5
Tomdieu <ivantomdio@gmail.com>
2025-02-28 23:26:37 +0100

    feat: update PDFDisplay component to use default layout plugin and external worker URL

--- frontend/src/components/PDFDisplay.tsx
+++ frontend/src/components/PDFDisplay.tsx
@@ -1,18 +1,29 @@
 "use client";
 import React from "react";
-import { Worker, Viewer } from "@react-pdf-viewer/core";
+// import { Worker, Viewer } from "@react-pdf-viewer/core";
+// import "@react-pdf-viewer/core/lib/styles/index.css";
+
+import { Viewer, Worker } from "@react-pdf-viewer/core";
+import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
+
 import "@react-pdf-viewer/core/lib/styles/index.css";
+import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 
 type PDFDisplayProps = {
   pdfUrl: string;
 };
 
 const PDFDisplay: React.FC<PDFDisplayProps> = ({ pdfUrl }) => {
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
+
   return (
     <div className="pdf-container">
-      <Worker workerUrl={window.location.origin+"/pdf.worker.min.mjs"}>
-        <div style={{ height: '750px' }}>
-          <Viewer fileUrl={pdfUrl} />
+      <Worker
+        workerUrl="https://unpkg.com/pdfjs-dist@2.15.349/build/pdf.worker.js"
+        // workerUrl={window.location.origin+"/pdf.worker.min.mjs"}
+      >
+        <div style={{ height: "750px" }}>
+          <Viewer fileUrl={pdfUrl} plugins={[defaultLayoutPluginInstance]} />
         </div>
       </Worker>
     </div>
