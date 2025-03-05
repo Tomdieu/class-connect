cf5bcd92cf1a476c02614279c5f4f9c71b25be6a
Tomdieu <ivantomdio@gmail.com>
2025-03-05 08:19:03 +0100

    refactor: remove unnecessary console log from PDFDialog component

--- frontend/src/components/dashboard/admin/modals/PDFDialog.tsx
+++ frontend/src/components/dashboard/admin/modals/PDFDialog.tsx
@@ -5,8 +5,7 @@ import { VisuallyHidden } from '@radix-ui/react-visually-hidden';
 
 import { useViewPDFStore } from "@/hooks/view-pdf-store";
 export function PDFDialog() {
-  const { isOpen, setIsOpen, pdfUrl,setPdfUrl } = useViewPDFStore();
-  console.log({isOpen, pdfUrl})
+  const { isOpen, setIsOpen, pdfUrl } = useViewPDFStore();
 
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
