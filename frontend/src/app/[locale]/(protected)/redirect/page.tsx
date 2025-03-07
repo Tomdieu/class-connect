a60d4a2a77f4d680fc698d9e454bb549a4e2915b
Tomdieu <ivantomdio@gmail.com>
2025-03-07 18:15:46 +0100

    feat: add ProtectedPage component for role-based redirection

---
+++ frontend/src/app/[locale]/(protected)/redirect/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { getUserRole } from "@/lib/utils";
+import { UserType } from "@/types";
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
+import React, { useEffect } from "react";
+
+function ProtectedPage() {
+  const { data: session } = useSession();
+  useEffect(() => {
+    // You can add additional role checking here if needed
+    if (session?.user) {
+      if (getUserRole(session?.user as UserType) === "student") {
+        redirect("/students");
+      }
+      if (getUserRole(session?.user as UserType) === "teacher") {
+        redirect("/dashboard");
+      }
+      if (getUserRole(session?.user as UserType) === "admin") {
+        redirect("/admin");
+      }
+    }
+  }, [session?.user]);
+  return <React.Fragment></React.Fragment>;
+}
+
+export default ProtectedPage;
