2845de715e315fc719c45563ba39904b7f3a1969
Tomdieu <ivantomdio@gmail.com>
2025-03-14 19:59:38 +0100

    feat: update OAuth2 configuration with explicit namespace and application name

--- backend/monolythic/backend/urls.py
+++ backend/monolythic/backend/urls.py
@@ -1,18 +1,5 @@
 """
 URL configuration for backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path,include,re_path
@@ -44,7 +31,9 @@ urlpatterns = [
         path('',include('notifications.urls')),
         path('',include('streamings.urls')),
         
-        re_path(r'^auth/', include('drf_social_oauth2.urls', namespace='drf')),
+        # Updated OAuth2 configuration with explicit namespace and application name
+        re_path(r'^auth/', include(('drf_social_oauth2.urls', 'drf_social_oauth2'), namespace='drf')),
+        
         path(
         "docs/",
         schema_view.with_ui("swagger", cache_timeout=0),
