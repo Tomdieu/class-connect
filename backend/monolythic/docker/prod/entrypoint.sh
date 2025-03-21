4eb63ceff92d1c76dc67c84ceb0eeb2902b17605
Tomdieu <ivantomdio@gmail.com>
2025-03-21 13:48:26 +0100

    feat: add migration check for Django Celery Beat in entrypoint script

--- backend/monolythic/docker/prod/entrypoint.sh
+++ backend/monolythic/docker/prod/entrypoint.sh
@@ -28,4 +28,37 @@ sleep 1
 done
 >&2 echo "PostgreSQL is ready!!!!.....:-)"
 
+# Check if migrations are complete by verifying if django_celery_beat_periodictask table exists
+migrations_complete() {
+python << END
+import sys
+import psycopg2
+try:
+   conn = psycopg2.connect(
+      dbname="${POSTGRES_DB}",
+      user="${POSTGRES_USER}",
+      password="${POSTGRES_PASSWORD}",
+      host="${POSTGRES_HOST}",
+      port="${POSTGRES_PORT}",
+   )
+   cursor = conn.cursor()
+   cursor.execute("SELECT to_regclass('public.django_celery_beat_periodictask');")
+   result = cursor.fetchone()[0]
+   if result is None:
+      sys.exit(-1)
+except Exception:
+   sys.exit(-1)
+sys.exit(0)
+END
+}
+
+# For Celery Beat, wait until migrations are complete
+if [[ "$@" == *"celery -A backend beat"* ]]; then
+  until migrations_complete; do
+    >&2 echo "Waiting for Django migrations to be applied......"
+    sleep 3
+  done
+  >&2 echo "Migrations are complete! Starting Celery Beat......"
+fi
+
 exec "$@"
