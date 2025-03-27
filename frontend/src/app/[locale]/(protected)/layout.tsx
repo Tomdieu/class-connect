ea28885b8ee14f2029df5bc4576295bc168c3ee4
Tomdieu <ivantomdio@gmail.com>
2025-03-27 23:38:19 +0100

    refactor: remove InactivityProvider from ProtectedLayout component

--- frontend/src/app/[locale]/(protected)/layout.tsx
+++ frontend/src/app/[locale]/(protected)/layout.tsx
@@ -1,11 +1,10 @@
 "use client";
-import InactivityProvider from "@/providers/InactivityProvider";
 import React, { PropsWithChildren } from "react";
 
 function ProtectedLayout({ children }: PropsWithChildren) {
   return (
     <React.Fragment>
-      <InactivityProvider>{children}</InactivityProvider>
+      {children}
     </React.Fragment>
   );
 }
