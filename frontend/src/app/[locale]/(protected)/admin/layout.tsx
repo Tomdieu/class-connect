6ea6f81ae9b4e07fba413202fd0b7c8e1eef29ab
Tomdieu <ivantomdio@gmail.com>
2025-03-27 23:32:55 +0100

    feat: enhance AdminLayout to improve sidebar structure and enable better content overflow handling

--- frontend/src/app/[locale]/(protected)/admin/layout.tsx
+++ frontend/src/app/[locale]/(protected)/admin/layout.tsx
@@ -83,10 +83,12 @@ export default function AdminLayout({ children }: React.PropsWithChildren) {
       className="h-full flex w-full"
     >
       <div className="flex flex-1 w-full">
-        <AdminSidebar />
-        <main className="flex flex-col flex-1 h-full w-full">
+        <div className="z-30">
+          <AdminSidebar />
+        </div>
+        <main className="flex flex-col flex-1 h-full w-full z-10 relative">
           <DashboardHeader />
-          <div className="flex-1 overflow-auto">{children}</div>
+          <div className="flex-1 overflow-y-auto overflow-x-hidden">{children}</div>
         </main>
       </div>
     </SidebarProvider>
