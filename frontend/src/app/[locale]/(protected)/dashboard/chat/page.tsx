3b97b208b87e0a0cfb7bcfec6e3effc0750a8b53
Tomdieu <ivantomdio@gmail.com>
2025-03-31 22:44:23 +0100

    style: format code in ChatPage component for improved readability

--- frontend/src/app/[locale]/(protected)/dashboard/chat/page.tsx
+++ frontend/src/app/[locale]/(protected)/dashboard/chat/page.tsx
@@ -1,19 +1,16 @@
-"use client"
+"use client";
 
-import { ChatSidebar } from "@/components/dashboard/chat/ChatSidebar"
-import { ChatWindow } from "@/components/dashboard/chat/ChatWindow"
-import { ChatProvider } from "@/contexts/ChatContext"
-
+import { ChatSidebar } from "@/components/dashboard/chat/ChatSidebar";
+import { ChatWindow } from "@/components/dashboard/chat/ChatWindow";
+import { ChatProvider } from "@/contexts/ChatContext";
 
 export default function ChatPage() {
-  
-  
   return (
     <ChatProvider>
-        <div className="flex h-[calc(100vh-4rem)] bg-background">
-          <ChatSidebar />
-          <ChatWindow />
-        </div>
-      </ChatProvider>
-  )
+      <div className="flex h-[calc(100vh-4rem)] bg-background">
+        <ChatSidebar />
+        <ChatWindow />
+      </div>
+    </ChatProvider>
+  );
 }
