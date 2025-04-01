5c582a8013ef6b9f9f88066eee254fb44bc65a4d
Tomdieu <ivantomdio@gmail.com>
2025-04-01 11:34:49 +0100

    feat: comment out cache_page decorators in viewsets for performance adjustments

--- backend/monolythic/courses/views.py
+++ backend/monolythic/courses/views.py
@@ -29,7 +29,7 @@ from .filters import (
     TeacherStudentEnrollmentFilter, CourseDeclarationFilter, UserClassFilter
 )
 from django.utils.decorators import method_decorator
-from django.views.decorators.cache import cache_page
+# from django.views.decorators.cache import cache_page
 from rest_framework import serializers
 
 class CourseCategoryViewSet(viewsets.ModelViewSet):
@@ -262,11 +262,11 @@ class ClassViewSet(viewsets.ModelViewSet):
 
         return Response(formatted_data)
     
-    @method_decorator(cache_page(60*60*2,key_prefix='class_list'))
+    # @method_decorator(cache_page(60*60*2,key_prefix='class_list'))
     def list(self, request, *args, **kwargs):
         return super().list(request, *args, **kwargs)
     
-    @method_decorator(cache_page(60*60*2,key_prefix='class_detail'))
+    # @method_decorator(cache_page(60*60*2,key_prefix='class_detail'))
     def retrieve(self, request, *args, **kwargs):
         return super().retrieve(request, *args, **kwargs)
 
@@ -350,7 +350,7 @@ class SubjectViewSet(viewsets.ModelViewSet):
         queryset = Subject.objects.filter(class_level=self.kwargs['class_pk'])
         return self.filterset_class(self.request.GET, queryset=queryset).qs
 
-    @method_decorator(cache_page(60*60*2,key_prefix='subjects_list'))
+    # @method_decorator(cache_page(60*60*2,key_prefix='subjects_list'))
     def list(self, request, *args, **kwargs):
         return super().list(request, *args, **kwargs)
 
