bc99508c637c4b1399316c0843d60c24f7563eec
Tomdieu <ivantomdio@gmail.com>
2025-04-04 00:10:51 +0100

    feat: expand UserParams interface with additional education-related fields

--- frontend/src/types/params.d.ts
+++ frontend/src/types/params.d.ts
@@ -5,6 +5,11 @@ declare interface UserParams {
     name?:string,
     phone_number?:string,
     education_level?:string,
+    college_class?:string;
+    lycee_class?:string;
+    lycee_speciality?:string;
+    university_level?:string;
+    university_year?:string;
     is_active?:boolean,
     date_joined?:string,
     language?:string,
@@ -13,4 +18,10 @@ declare interface UserParams {
     is_student?:boolean;
     is_professional?:boolean;
     is_admin?:boolean;
+
+    has_subscription?:boolean;
+    subscription_expiring?:boolean;
+    subscription_expired?:boolean;
+
+    subscription_plan?: "BASIC"|"STANDARD"|"PREMIUM"
 }
