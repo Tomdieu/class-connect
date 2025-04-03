20b375d5f535838e8a9bd9aca6f6f129a14100b5
Tomdieu <ivantomdio@gmail.com>
2025-04-04 00:13:04 +0100

    feat: add subscription filters and user distribution chart to users page

--- frontend/src/app/[locale]/(protected)/admin/users/page.tsx
+++ frontend/src/app/[locale]/(protected)/admin/users/page.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import UserTable from "./user-table";
 import UserTypeSidebar from "./_components/user-type-sidebar";
+import SubscriptionFilters from "./_components/subscription-filters";
+import UserDistributionChart from "./_components/user-distribution-chart";
+import EducationLevelFilters from "./_components/education-level-filters";
 
 async function UsersPage() {
   return (
@@ -10,6 +13,11 @@ async function UsersPage() {
           <UserTypeSidebar />
         </div>
         <div className="w-full lg:flex-1">
+          <UserDistributionChart />
+          <div className="space-y-4 mb-4">
+            <SubscriptionFilters />
+            <EducationLevelFilters />
+          </div>
           <UserTable />
         </div>
       </div>
