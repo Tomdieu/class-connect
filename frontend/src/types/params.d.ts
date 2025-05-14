33c8c8ba9f82f1c3252552714b70c446e8014954
Tomdieu <ivantomdio@gmail.com>
2025-05-14 19:32:04 +0100

    refactor: reorganize UserParams interface by removing unused education fields

--- frontend/src/types/params.d.ts
+++ frontend/src/types/params.d.ts
@@ -4,12 +4,6 @@ declare interface UserParams {
     last_name?:string,
     name?:string,
     phone_number?:string,
-    education_level?:string,
-    college_class?:string;
-    lycee_class?:string;
-    lycee_speciality?:string;
-    university_level?:string;
-    university_year?:string;
     is_active?:boolean,
     date_joined?:string,
     language?:string,
@@ -18,6 +12,10 @@ declare interface UserParams {
     is_student?:boolean;
     is_professional?:boolean;
     is_admin?:boolean;
+
+    section?:string;
+    education_level?:string;
+    class_name?:string;
 
     has_subscription?:boolean;
     subscription_expiring?:boolean;
